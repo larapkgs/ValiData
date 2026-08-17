38ca643ec69ae2b293b29d7136e276e6458b887b
LaraPkgs <mail@larapkgs.com>
2026-08-17 14:34:37 +0200

    refactor: change Snapshot constructor scheme type tp Schema interface

--- src/Snapshot.php
+++ src/Snapshot.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace LaraPkgs\ValiData;
 
 use LaraPkgs\ValiData\Concerns\HasValidData;
+use LaraPkgs\ValiData\Contracts\Schema;
 use LaraPkgs\ValiData\Contracts\ValidData;
 
 final class Snapshot implements ValidData
@@ -14,7 +15,7 @@ final class Snapshot implements ValidData
     /**
      * @param  array<array-key, mixed>  $payload
      */
-    public function __construct(array $payload, ?SchemaBuilder $schema = null)
+    public function __construct(array $payload, ?Schema $schema = null)
     {
         $this->applyPayload($payload, $schema);
     }
